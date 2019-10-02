a26f06f128fd874d7b9c1ac51b38c857f4b66a6e
Micheal Koblinski <stevenkoblinskip@gmail.com>
2019-10-02 14:12:20 -0400

    Added timer

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
-console.log('Hello world')
+{console.log('Hello world')
+let time, seconds;
 
 const DOMS = {
     startBtn: "#btn-start",
     hours: "#hours",
     minutes: "#minutes",
     seconds: "#seconds",
+}
+
+const countSec = () => {
+    seconds++;
+}
+
+const updateTimer = (sec) => {
+    document.querySelector(DOMS.seconds).textContent = sec;
+}
+
+const setupEventListeners = () => {
+    seconds = 0;
+    document.querySelector(DOMS.startBtn).addEventListener('click', () => {
+        timer();
+    });
+}
+
+const timer = () => {
+    time = requestAnimationFrame(timer);
+    updateTimer(Math.floor(seconds/60));
+    countSec();
+    console.log(Math.floor(seconds/60));
+}
+
+setupEventListeners();
 }

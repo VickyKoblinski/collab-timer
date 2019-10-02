4d25c752954fa11990862cbee140ab34016060b7
Micheal Koblinski <stevenkoblinskip@gmail.com>
2019-10-02 15:05:33 -0400

    Formated Seconds

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
 {console.log('Hello world')
-let time, seconds;
+let time, seconds, fullSec, miuntes;
 
 const DOMS = {
     startBtn: "#btn-start",
     hours: "#hours",
-    minutes: "#minutes",
+    minute: "#minutes",
     seconds: "#seconds",
 }
 
+const formatNumber = (number, time) =>
+{
+    if (number < 10) {
+        document.querySelector(time).textContent = `0${number}`;
+    }
+}
+
 const countSec = () => {
+    fullSec = Math.floor(seconds/20)
     seconds++;
+    if (fullSec === 15){
+        seconds = 0;
+    }
 }
 
 const updateTimer = (sec) => {
     document.querySelector(DOMS.seconds).textContent = sec;
+    formatNumber(fullSec, DOMS.seconds);
+    if (sec === 14){
+        miuntes++;
+        document.querySelector(DOMS.minute).textContent = Math.floor(miuntes/20);
+        formatNumber(minutes, DOMS.minute);
+        console.log(minutes)
+    }
 }
 
 const setupEventListeners = () => {
     seconds = 0;
+    miuntes = 0;
     document.querySelector(DOMS.startBtn).addEventListener('click', () => {
         timer();
     });
@@ -25,9 +44,9 @@ const setupEventListeners = () => {
 
 const timer = () => {
     time = requestAnimationFrame(timer);
-    updateTimer(Math.floor(seconds/60));
+    updateTimer(fullSec);
     countSec();
-    console.log(Math.floor(seconds/60));
+    console.log(fullSec);
 }
 
 setupEventListeners();

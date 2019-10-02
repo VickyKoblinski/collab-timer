1591eac6814138fb93f14b9f0dbaa6f58df27997
Micheal Koblinski <stevenkoblinskip@gmail.com>
2019-10-02 16:20:07 -0400

    Adding the hours and minutes to the stop watch

--- main.js
+++ main.js
@@ -1,52 +1,79 @@
 {console.log('Hello world')
-let time, seconds, fullSec, miuntes;
+let time, seconds, fullSec, fullMin, fullHour, clicked, turned;
+const frame = 60;
 
 const DOMS = {
     startBtn: "#btn-start",
     hours: "#hours",
-    minute: "#minutes",
+    minutes: "#minutes",
     seconds: "#seconds",
 }
 
 const formatNumber = (number, time) =>
 {
-    if (number < 10) {
-        document.querySelector(time).textContent = `0${number}`;
+    if (typeof(number) === 'number' && !NaN) {
+        if (number < 10) {
+            document.querySelector(time).textContent = `0${number}`;
+        } else {
+            document.querySelector(time).textContent = `${number}`;
+        }
+    } else {
+        document.querySelector(time).textContent = `00`;
     }
 }
 
 const countSec = () => {
-    fullSec = Math.floor(seconds/20)
+    fullSec = Math.floor(seconds/frame)
     seconds++;
-    if (fullSec === 15){
+    if (fullSec === 60){
         seconds = 0;
     }
+    formatNumber(fullSec, DOMS.seconds);
 }
 
-const updateTimer = (sec) => {
-    document.querySelector(DOMS.seconds).textContent = sec;
-    formatNumber(fullSec, DOMS.seconds);
-    if (sec === 14){
-        miuntes++;
-        document.querySelector(DOMS.minute).textContent = Math.floor(miuntes/20);
-        formatNumber(minutes, DOMS.minute);
-        console.log(minutes)
+const countMin = () => {
+    fullMin++;
+    turned = false;
+    if (fullMin === 60) {
+        fullMin = 0;
+        turned = true;
     }
+    formatNumber(fullMin, DOMS.minutes);
+}
+const countHour = () => {
+    fullHour++;
+    if (fullHour === 24) {
+        fullHour = 0;
+    }
+    formatNumber(fullHour, DOMS.hours);
 }
 
 const setupEventListeners = () => {
     seconds = 0;
     miuntes = 0;
+    fullMin = 0;
+    fullHour = 0;
+    clicked = false;
     document.querySelector(DOMS.startBtn).addEventListener('click', () => {
-        timer();
+        if(!clicked){ 
+            timer();
+        }
+        clicked = true;
     });
 }
 
 const timer = () => {
     time = requestAnimationFrame(timer);
-    updateTimer(fullSec);
     countSec();
-    console.log(fullSec);
+    if(fullSec === 60) {
+        countMin();
+        if (turned) {
+            countHour();
+        }
+    }
+   // console.log(fullSec);
+    //console.log(fullMin);
+    console.log(fullHour);
 }
 
 setupEventListeners();
